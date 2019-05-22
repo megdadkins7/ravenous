736ee16d6d46eb147d0301854bdaf913dd9c3cbc
Meghan Adkins <meghandadkins@gmail.com>
2019-05-22 10:58:51 -0400

    DRYed up code

--- src/App.js
+++ src/App.js
@@ -5,13 +5,27 @@ import './components/SearchBar/SearchBar';
 import SearchBar from './components/SearchBar/SearchBar';
 import BusinessList from './components/BusinessList/BusinessList';
 
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+const businesses = [business, business, business, business, business, business];
+
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
           <SearchBar />
-          <BusinessList /> 
+          <BusinessList business={businesses} /> 
       </div>
     );
   }
