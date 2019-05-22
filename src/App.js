003649efee2790871bab74bf7d7750f98cab10b0
Meghan Adkins <meghandadkins@gmail.com>
2019-05-22 11:04:58 -0400

    Fixed errors

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
 import React from 'react';
 import './App.css';
-import './components/BusinessList/BusinessList';
-import './components/SearchBar/SearchBar';
-import SearchBar from './components/SearchBar/SearchBar';
-import BusinessList from './components/BusinessList/BusinessList';
+
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
 
 const business = {
   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
   name: 'MarginOtto Pizzeria',
   address: '1010 Paddington Way',
-  city: 'Flavortown',
+  city: 'Bordertown',
   state: 'NY',
   zipCode: '10101',
   category: 'Italian',
   rating: 4.5,
   reviewCount: 90
-}
+};
 
-const businesses = [business, business, business, business, business, business];
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+];
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
-          <SearchBar />
-          <BusinessList business={businesses} /> 
+        <SearchBar />
+        <BusinessList businesses={businesses} />
       </div>
     );
   }
